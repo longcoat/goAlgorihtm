0ae1f9adfec801c2bc36fe8b464a89a113b2b0a3
longcoat <sherlockpkm@gmail.com>
2023-03-10 10:25:12 +0900

    20230310 solution 1st

--- pages/20230303.피자나눠먹기(1).js
+++ pages/20230303.피자나눠먹기(1).js
@@ -1,7 +1,7 @@
 function solution(n) {
   let answer = 0;
 
-  answer = n % 7 === 0 ? Math.floor(n / 7) : Math.floor(n / 7) + 1;
+  answer = n % 7 === 0 ? n / 7 : Math.floor(n / 7) + 1;
 
   return answer;
 }
