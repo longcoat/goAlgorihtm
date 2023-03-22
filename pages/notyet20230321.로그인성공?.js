a4e531f932b83d232130befd0f5a37b65d408072
longcoat <sherlockpkm@gmail.com>
2023-03-22 13:19:58 +0900

    20230322 solution solved

--- pages/notyet20230321.로그인성공?.js
+++ pages/notyet20230321.로그인성공?.js
@@ -1,10 +1,8 @@
 function solution(idpw, db) {
   let matchId = db.filter((el) => el[0] === idpw[0]);
-  let matchPw = db.filter((el) => el[1] === idpw[1]);
+  let matchPw = matchId.filter((el) => el[1] === idpw[1]);
   // console.log(idpw)
   if (!!matchPw.length) return "login";
   else if (!!matchId.length) return "wrong pw";
   else return "fail";
 }
-
-// 다시생각해보자
