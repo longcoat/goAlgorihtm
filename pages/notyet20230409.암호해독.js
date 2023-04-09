df5924b1b0450f1bbeee9282b006ff4b054e64d2
longcoat <sherlockpkm@gmail.com>
2023-04-09 10:38:46 +0900

    20230409 solution not yet 1st

---
+++ pages/notyet20230409.암호해독.js
@@ -0,0 +1,8 @@
+function solution(cipher, code) {
+  // let answer = [...cipher].filter((el) => )
+  let answer = cipher.split("");
+  // console.log(cipher.split(""))
+  // console.log([...cipher]
+}
+
+// 좀만 더 생각해보자
