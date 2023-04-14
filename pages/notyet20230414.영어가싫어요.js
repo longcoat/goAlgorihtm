292b33004343614422055a01c2282f8e0c323cdc
longcoat <sherlockpkm@gmail.com>
2023-04-14 14:58:49 +0900

    20230414 solution not yet 1st

---
+++ pages/notyet20230414.영어가싫어요.js
@@ -0,0 +1,19 @@
+function solution(numbers) {
+  let numString = [
+    "zero",
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+  ];
+  let answer = numbers.forEach((el, index) => el);
+  // replace???
+  // replaceAll???? 뭘 어떻게 대체 써야하는 걸까?
+}
+
+// 다시 생각해보자
