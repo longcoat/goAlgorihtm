57a65c18ff4cc32bc628a45ccf9f6895db6adec3
longcoat <sherlockpkm@gmail.com>
2023-04-22 18:22:49 +0900

    20230422 solution solved 1st

--- pages/notyet20230421.숨어있는숫자의덧셈(2).js
+++ pages/notyet20230421.숨어있는숫자의덧셈(2).js
@@ -1,12 +1,19 @@
 function solution(str) {
   let sum = 0;
   let num = "";
+
   for (let i = 0; i < str.length; i++) {
     if (str[i] >= "0" && str[i] <= "9") {
-      num += parseInt(str[i]);
-      console.log(num);
+      num += str[i];
+    } else if (num !== "") {
+      sum += parseInt(num);
+      num = "";
     }
   }
+  if (num !== "") {
+    sum += parseInt(num);
+  }
+  return sum;
 }
 
-//  parseInt 저기서 쓰는 것 좀 아닌 것 같은데 다시 한번 생각해보자
+// 다시 풀라고 하면 못 풀 것 같은데 일단 다시 한번 생각해보자
